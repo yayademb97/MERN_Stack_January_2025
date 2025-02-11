d3cd3c29b938c4633af1619c97a47f05772bdfa4
yayademb97 <yayadembele09@gmail.com>
2025-02-11 15:30:50 +0100

    Push it

---
+++ JavaScript/Fundamentals/Convert To Arrow/index.js
@@ -0,0 +1,8 @@
+console.log(Greeting('John Doe')); // 
+function Greeting (name)  {
+    return `Hello ${name}`;
+}
+
+
+console.log(GrettingOne('Jane Doe')); //
+const GrettingOne = (name) => `Hello ${name}`;
